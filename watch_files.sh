4bd901c9b42716d8e3ca4044183ad598b1c68d4a
Lars Grahmann <lars@kloud.software>
2019-11-04 11:26:07 +0100

    delete aux files after build

--- watch_files.sh
+++ watch_files.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+
+function del {
+	rm -rf auto/
+	find . -name '*.aux' -delete
+	find . -name '*.log' -delete
+	find . -name '*.nav' -delete
+	find . -name '*.out' -delete
+	find . -name '*.snm' -delete
+	find . -name '*.synctex.gz' -delete
+	find . -name '*.toc' -delete
+	find . -name '*.blg' -delete
+	find . -name '*.fdb_latexmk' -delete
+	find . -name '*.fls' -delete
+	find . -name '*.bcf' -delete
+	find . -name '*.bbl' -delete
+	find . -name '*.run.xml' -delete
+}
+
 latexmk -C
 biber main
 latexmk -cd -interaction=nonstopmode -pdf main.tex
+del
 inotifywait -m -r -e close_write --format '%w%f' "." | while read MODIFIED
 do
     if [[ $MODIFIED == *.tex ]] ;
@@ -9,5 +28,7 @@ do
 	latexmk -c
 	biber main
         latexmk -cd -interaction=nonstopmode -pdf main.tex
+	latexmk -c
+	del
     fi
 done
